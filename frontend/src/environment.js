da998c3a263545a8e11daef78ddfdfb84842abda
Nabin Bista <bistanabin98@gmail.com>
2025-07-09 01:57:08 +0545

    fixed frontend api path for deploy

---
+++ frontend/src/environment.js
@@ -0,0 +1,7 @@
+
+const server = {
+    dev: "http://localhost:3000",
+    prod: "https://note-nfsc.onrender.com"
+}
+
+export default server;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import server from "./src/environment";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/v1/": {
-        target: "http://localhost:3000/api",
+        // target: "http://localhost:3000/api",
+        target: `${server.prod}/api`,
         changeOrigin: true,
         secure: true,
       },
